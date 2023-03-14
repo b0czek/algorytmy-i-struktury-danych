356de75868ee12afa68963c0c6de31caf52f1896
Dariusz Majnert <dariusz.majnert@gmail.com>
2023-03-14 09:54:04 +0100

    fix

--- lista1/src/Zad2.java
+++ lista1/src/Zad2.java
@@ -1,8 +1,10 @@
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Zad2 {
 
     private static final int[][] testCases = new int[][]{
+            null,
             {},
             {6},
             {1, 2, 3},
@@ -10,9 +12,11 @@ public class Zad2 {
             {-1, 1, -2, 2},
             {1, 2, 8, -3, -3, -4, 7, 10, 0},
             {7, 7, 7, 7, 8, 2, 5, 5, 5, 10},
+            {-3, -2, -1, -2, -1, 0, 1, 2, 3, -5, -7},
             {1, 2, 3, 4, 5, 2, 3, 4, -1, -2, -3, -4, 245, 245, 245, 566}
     };
     private static final int[][][] expectedOutput = new int[][][]{
+            null,
             {{}},
             {{6}},
             {{1, 2, 3}},
@@ -20,10 +24,12 @@ public class Zad2 {
             {{-1, 1}, {-2, 2}},
             {{1, 2, 8}, {-3, -3}, {-4, 7, 10}, {0}},
             {{7, 7, 7, 7, 8}, {2, 5, 5, 5, 10}},
+            {{-3,-2,-1}, {-2,-1,0,1,2,3}, {-5}, {-7}},
             {{1, 2, 3, 4, 5}, {2, 3, 4}, {-1}, {-2}, {-3}, {-4, 245, 245, 245, 566}}
 
     };
     private static final Substring[] expectedOutput2 = new Substring[]{
+            null,
             new Substring(-1, 0),
             new Substring(0, 1),
             new Substring(0, 3),
@@ -31,6 +37,7 @@ public class Zad2 {
             new Substring(0, 2),
             new Substring(0, 3),
             new Substring(0, 5),
+            new Substring(3, 6),
             new Substring(0, 5)
 
     };
@@ -48,6 +55,9 @@ public class Zad2 {
     }
 
     public static int[][] longestNondecreasingSubstrings(int[] numbers) {
+        if(numbers == null) {
+            return null;
+        }
         if (numbers.length <= 1) {
             return new int[][]{numbers};
         }
@@ -67,6 +77,9 @@ public class Zad2 {
     }
 
     public static Substring longestNondecreasingSubstring(int[] numbers) {
+        if(numbers == null) {
+            return null;
+        }
         if (numbers.length == 0) {
             return new Substring(-1, 0);
         }
@@ -79,20 +92,15 @@ public class Zad2 {
 
         for (int i = 1; i < numbers.length; i++) {
             if (numbers[i - 1] > numbers[i]) {
-                if(length > maxLength) {
-                    maxPos = pos;
-                    maxLength = length;
-                }
-                pos = i + 1;
+                pos = i;
                 length = 1;
-            }
-            else {
+            } else {
                 length++;
             }
-        }
-        if(length > maxLength) {
-            maxPos = pos;
-            maxLength = length;
+            if (length > maxLength) {
+                maxPos = pos;
+                maxLength = length;
+            }
         }
 
         return new Substring(maxPos, maxLength);
@@ -112,7 +120,7 @@ public class Zad2 {
             {
                 var result = longestNondecreasingSubstring(testCases[i]);
                 System.out.println("test2 result: " + result);
-                boolean valid = result.equals(expectedOutput2[i]);
+                boolean valid = Objects.equals(expectedOutput2[i], result);
                 System.out.println(valid ? "pass" : "fail");
                 System.out.println();
             }
